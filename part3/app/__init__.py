2fdac413f5105964910ab498dbe1422510dac5f8
SpacePistache <lukeholley@hotmail.com>
2025-03-11 11:10:25 +0100

    Updated init.py w/ sqlalc import files + config.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -4,13 +4,16 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
+db = SQLAlchemy()
 bcrypt = Bcrypt()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    from flask_sqlalchemy import SQLAlchemy
     bcrypt.init_app(app)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
