c4c7cf1aba8c07434457ec265bb1b0bd84b3e2f9
SpacePistache <lukeholley@hotmail.com>
2025-03-11 15:16:26 +0100

    changec user + basemodel ORM functionality

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,19 +1,18 @@
 from .basemodel import BaseModel
 import re
 import bcrypt
+from app import db
 
 class User(BaseModel):
-    emails = set()
+    
+    __tablename__ = 'users'
 
-    def __init__(self, first_name, last_name, email, password, is_admin=False):
-        super().__init__()
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.password = password
-        self.is_admin = is_admin
-        self.places = []
-        self.reviews = []
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    
     
     @property
     def first_name(self):
