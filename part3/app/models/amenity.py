54a80b6bbd8ccacc11a7ecde8d2acf59938e3816
SpacePistache <lukeholley@hotmail.com>
2025-03-15 09:03:38 +0100

    added join function for many to many implementation

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -1,35 +1,45 @@
 from .basemodel import BaseModel
 from app import db
 
+place_amenity = db.Table(
+    'place_amenity',
+    db.Column('place_id', db.Integer, db.ForeignKey('places.id'), primary_key=True),
+    db.Column('amenity_id', db.Integer, db.ForeignKey('amenities.id'), primary_key=True)
+)
+
+
 class Amenity(BaseModel):
 
     __tablename__ = 'amenities'
-id = db.Column(db.Integer, primary_key=True)
-name = db.Column(db.String(50), nullable=False)
-_is_admin = db.Column(db.Boolean, default=False)
 
-def __init__(self, name):
-		super().__init__()	
-		self.name = name
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
 
-@property
-def name(self):
-		return self.__name
 
-@name.setter
-def name(self, value):
-	if not isinstance(value, str):
-			raise TypeError("Name must be a string")
-	if not value:
-			raise ValueError("Name cannot be empty")
-	super().is_max_length('Name', value, 50)
-	self.__name = value
+    places = db.relationship('Place', secondary=place_amenity, back_populates='amenities')
 
-def update(self, data):
-	return super().update(data)
-	
-def to_dict(self):
-    return {
+    
+    def to_dict(self):
+        return {
 			'id': self.id,
 			'name': self.name
 		}
+
+
+
+    @property
+    def name(self):
+       return self.__name
+
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string")
+        if not value:
+            raise ValueError("Name cannot be empty")
+        super().is_max_length('Name', value, 50)
+        self.__name = value
+
+    def update(self, data):
+	    return super().update(data)
