2c571dceeddf859d45a573e59720fa7dae071df3
turtle-nest <n78693107@gmail.com>
2025-03-16 14:55:32 +0100

    Add relationship and fixed file.

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -1,6 +1,12 @@
 from app.models.basemodel import BaseModel
 from app import db
 
+place_amenity = db.Table(
+    'place_amenity',
+    db.Column('place_id', db.Integer, db.ForeignKey('places.id'), primary_key=True),
+    db.Column('amenity_id', db.Integer, db.ForeignKey('amenities.id'), primary_key=True)
+)
+
 class Amenity(BaseModel):
     """Amenity model representing facilities available at a place."""
     __tablename__ = 'amenities'
@@ -8,12 +14,10 @@ class Amenity(BaseModel):
     id = db.Column(db.Integer, primary_key=True)  # Primary key
     name = db.Column(db.String(50), nullable=False, unique=True)
 
+    places = db.relationship('Place', secondary=place_amenity, back_populates='amenities')
 
-    def __init__(self, name):
-        """Initialize a new amenity with required attributes."""
-        super().__init__()
-        self.name = name
-
+    def update(self, data):
+        return super().update(data)
 
     def to_dict(self):
         """Return a dictionary representation of the amenity."""
