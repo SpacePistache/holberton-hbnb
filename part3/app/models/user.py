e0215d163055a04b1e0ecede95cdf9102256d9ed
turtle-nest <n78693107@gmail.com>
2025-03-16 14:27:34 +0100

    Change def to_dict and to_safe_dict with an _ and remove methods.

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,11 +1,10 @@
 from app.models.basemodel import BaseModel
 import re
 from sqlalchemy.orm import validates
-from sqlalchemy.ext.hybrid import hybrid_property
 from app import bcrypt
 from app import db
 
-class User(BaseModel):
+class User(BaseModel, db.Model):
     """User model representing application users."""
     __tablename__ = 'users'
 
@@ -14,18 +13,6 @@ class User(BaseModel):
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
-
-
-    def __init__(self, first_name, last_name, email, password, is_admin=False):
-        """Initialize a new user with required attributes."""
-        super().__init__()
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.password = password
-        self.is_admin = is_admin
-        self.places = []
-        self.reviews = []
 
 
     @validates('email')
@@ -41,42 +28,26 @@ class User(BaseModel):
         if len(password) < 8:
             raise ValueError("Password must be at least 8 characters long.")
         return password
-
-    @hybrid_property
-    def full_name(self):
-        """Return the user's full name."""
-        return f"{self.first_name} {self.last_name}"
-
-
-    def add_place(self, place):
-        """Associate a place with the user."""
-        self.places.append(place)
-
-    def add_review(self, review):
-        """Associate a review with the user."""
-        self.reviews.append(review)
-
-    def delete_review(self, review):
-        """Remove a review associated with the user."""
-        self.reviews.remove(review)
+    
 
     def to_dict(self):
         """Return a dictionary representation of the user including sensitive data."""
         return {
-            'id': self.id,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'email': self.email,
-            'password': self.password
+            '_id': self.id,
+            '_first_name': self.first_name,
+            '_last_name': self.last_name,
+            '_email': self.email,
+            '_password': self.password
         }
 
     def to_safe_dict(self):
         """Return a dictionary without the password field"""
         return {
-            'id': self.id,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'email': self.email
+            '_id': self.id,
+            '_first_name': self.first_name,
+            '_last_name': self.last_name,
+            '_email': self.email,
+            '_is_admin': self.is_admin
         }
 
     def hash_password(self, password):
